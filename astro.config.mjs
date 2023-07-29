2a671d57e7b35a659ee647069114cc9fc2e1af50
Sven Brieden <sven.brieden@gmail.com>
2023-07-29 06:23:55 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  //site: 'https://brieden.github.io',
-  //base: '/astro_try'
+  site: 'https://brieden.github.io',
+  base: '/astro_try'
 });
